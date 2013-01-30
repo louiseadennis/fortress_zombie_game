fa82e6eec946f666fded1c8bc1afdc223a6f5fd5
Louise Dennis <louise@dennis-sellers.com>
2013-01-30 20:35:30 +0000

    You can now move around the map.

--- www/main.php
+++ www/main.php
@@ -5,23 +5,42 @@
 # Connect to DB
 $mysql = mysql_connect("localhost", "urbandead", "purplecat");
 mysql_select_db("ud2");
+
+# By default we arrive at 5,5 - will need to randomize this in future.
+$cx = $_GET["cx"];
+$cy = $_GET["cy"];
+if ($cx == NULL) {
+   $cx = 5;
+   $cy = 5;
+}
 ?>
 <html>
 <head>
+<title>Urban Dead 2 - The City</title>
+
+<link rel="stylesheet" href="styles/default.css" type="text/css">
 </head>
 <body>
+<div class=main>
+<table><tr>
 <?php
 
-$res = mysql_query("select name from squares", $mysql);
-while($row = mysql_fetch_array($res, MYSQL_NUM))
+$res = mysql_query("select name, xcoord, ycoord from squares where xcoord between ($cx - 1) and  ($cx + 1) and ycoord between ($cy - 1) and ($cy + 1)", $mysql);
+$counter = 0;
+while($row = mysql_fetch_array($res))
 {
-  foreach ($row as $attribute)
-    print "{$attribute} ";
-    print "<br>";
+   print "<td class=b><a href=main.php?cx={$row["xcoord"]}&cy={$row["ycoord"]}>{$row["name"]}</a></td>";
+   $counter++;
+   if ($counter == 3) {
+      print "</tr></tr>";
+      $counter = 0;
+       }
 }
-print('hello');
+
 
 ?>
+</table>
+</div>
 </body>
 </html>
 
